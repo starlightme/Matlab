1b7e64dcd3c32be699f74109f056421f53014ae1
starlightme <lsk19940602@gmail.com>
2014-11-17 18:02:11 +0800

    更新
    上传代码，添加笔记

---
+++ 图像叠加/e5.m
@@ -0,0 +1,12 @@
+clear;
+clc;
+a=imread('man.jpg');
+b=imread('rice.jpg');
+%‘§∑÷≈‰±‰¡ø
+c=uint8(zeros(160,160));   
+for i=1:160
+    for j=1:160
+        c(i,j)=a(i,j)+b(i,j);
+    end
+end
+imshow(c);
